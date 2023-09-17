3c3d4cc2af64095c16a7ae0968a1b32941823631
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2023-09-17 18:40:25 +0530

    Initial Commit

---
+++ To-do-List/index.js
@@ -0,0 +1,113 @@
+let enterBtn = document.getElementById("searchbtn");
+
+let valueList = document.getElementById("mainList");
+
+let deleteBtn = document.getElementsByClassName("delete");
+
+let count = 0;
+let taskCounter = 0
+updateCounter()
+
+enterBtn.addEventListener("click", () => {
+  let value = document.getElementById("work").value;
+
+  if (value == "") {
+  } else {
+    let a = document.createElement("div");
+    a.id = count;
+    a.classList.add("listItem");
+    valueList.append(a);
+    let b = document.createElement("input");
+    b.type = "checkbox";
+    b.classList.add("tick");
+    b.id = count
+    a.append(b);
+    let c = document.createElement("li");
+    c.classList.add("listel");
+    c.id = count;
+    c.innerHTML = value;
+    a.append(c);
+    let d = document.createElement("button");
+    d.classList.add("delete");
+    d.id = count;
+    d.innerHTML = "X";
+    a.append(d);
+
+    document.getElementById("work").value = "";
+    count++;
+    taskCounter++
+    updateCounter()
+  }
+  
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    let value = document.getElementById("work").value;
+
+    if (value == "") {
+    } else {
+      let a = document.createElement("div");
+      a.id = count;
+      a.classList.add("listItem");
+      valueList.append(a);
+      let b = document.createElement("input");
+      b.type = "checkbox";
+      b.classList.add("tick");
+      b.id = count
+      a.append(b);
+      let c = document.createElement("li");
+      c.classList.add("listel");
+      c.id = count;
+      c.innerHTML = value;
+      a.append(c);
+      let d = document.createElement("button");
+      d.classList.add("delete");
+      d.id = count;
+      d.innerHTML = "X";
+      a.append(d);
+
+      document.getElementById("work").value = "";
+      count++
+      taskCounter++
+      updateCounter()
+      console.log(taskCounter)
+    }
+    
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    let elementId = event.target.id;
+
+    let a = valueList.children;
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i].id == elementId) {
+        valueList.removeChild(a[i]);
+        taskCounter--;
+        updateCounter()
+      }
+    }
+  }
+
+  
+  });
+
+  document.addEventListener("click", (event) => {
+    if ((event.target.class = "tick")) {
+      let elementIdChecked = event.target.id;
+      let a = valueList.children
+      for(let i = 0;i<a.length;i++){
+        if(a[i].id == elementIdChecked){
+            console.log()
+            a[i].firstElementChild.nextElementSibling.classList.toggle("crossed")
+        }
+      }
+    }})
+
+
+    function updateCounter(){
+        document.getElementById("counter").innerHTML = taskCounter
+    }
